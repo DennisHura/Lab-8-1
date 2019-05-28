2c9c124ef7837228cd35245e02f2027b700a5f5e
Денис Гура <denis777gura@gmail.com>
2019-05-28 12:39:18 +0300

    Добавьте файлы проекта.

---
+++ lab-8-1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace prog_4s_l8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> numbers = new Stack<string>();
+            StreamReader sr = new StreamReader("in.txt");
+            string row = sr.ReadLine();
+            string[] rowSP = row.Split(' ');
+            for (int i = 0; i < rowSP.Length; i++)
+            {
+                Console.Write(rowSP[i]);
+            }
+            Console.WriteLine("\n\n");
+
+            List<string> skobochki = new List<string>();
+
+            int left = 0, right = 0;
+
+            for (int i = 0; i < rowSP.Length; i++)
+            {
+                if (rowSP[i] == '('.ToString())
+                {
+                    skobochki.Add(rowSP[i]);
+                    left++;
+                }
+                else
+                {
+                    if (rowSP[i] == ')'.ToString())
+                    {
+                        skobochki.Add(rowSP[i]);
+                        right++;
+                    }
+                }
+            }
+            //foreach(string skobochka in skobochki)
+            //{
+            //    Console.WriteLine(skobochka);
+            //}
+            if (left == right)
+            {
+                Console.WriteLine("Скобочек в выражении поровну");
+            }
+            else
+            {
+                if (left > right)
+                {
+                    Console.WriteLine("'(' скобочек больше");
+                }
+                else
+                {
+                    Console.WriteLine("')' скобочек больше");
+                }
+            }
+        }
+    }
+}
---
+++ lab-8-1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("lab-8-1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab-8-1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e60fd23e-13c1-4db3-8d56-930310d7e080")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
